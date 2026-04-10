8234110ef6093b7fd10184995f446b8996bc3af3
pascal2590 <155544942+pascal2590@users.noreply.github.com>
2026-04-10 12:50:13 +0200

    Améliorations UI de la page accueil - Filtre & Liste - Barre de recherche

--- src/app/models/vehicule.model.ts
+++ src/app/models/vehicule.model.ts
@@ -1,12 +1,27 @@
 export interface Vehicule {
+
     idVehicule: number;
+
     marque: string;
+
     modele: string;
+
     annee?: number;
+
     kilometrage?: number;
+
     prix?: number;
+
     description?: string;
+
     typeOffre: 'Vente' | 'Location';
+
     disponible: boolean;
+
     dateAjout: string;
+
+    /* ✅ AJOUT POUR LA PHOTO */
+
+    imageUrl?: string;
+
 }
